d594175c60a84ab890fcef601a9df336ac8f7d19
Moksh1429 <moksh1429.becse24@chitkara.edu.in>
2025-02-11 18:10:18 +0530

    Solution to Check Triangle Validity

--- Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -3,15 +3,15 @@
 int main(){
     int a,b,c;
     scanf("%d%d%d",&a,&b,&c);
-    if{((a + b > c) && (a + c > b) && (c + b > a)){
+    if((a + b > c) && (a + c > b) && (c + b > a)){
         printf("Valid");
     }
     else{
         printf("Invalid");
     }
     }
-    return 0;
-}
+    
+
 // #include <stdio.h>
 
 // int main() {
