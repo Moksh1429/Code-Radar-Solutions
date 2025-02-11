0c97b7eb570cfa98b6ac459bad9fd9aa285335d1
Moksh1429 <moksh1429.becse24@chitkara.edu.in>
2025-02-12 01:04:03 +0530

    Solution to Traffic Light Simulation

---
+++ Traffic Light Simulation@@6788dd63490637cfc44e1ba9/code.c
@@ -0,0 +1,16 @@
+// Your code here...
+#include<stdio.h>
+int main(){
+    char ch;
+    scanf("%c",&ch);
+    if(ch=='R'){
+        printf("Stop");
+    }
+    else if(ch=='G'){
+        printf("Go");
+    }
+    else if(ch=='Y'){
+        printf("Slow Down");
+    }
+
+}
