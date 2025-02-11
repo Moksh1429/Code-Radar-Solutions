c0fb2b4dac224070991b6255d33bc569112dd297
Moksh1429 <moksh1429.becse24@chitkara.edu.in>
2025-02-11 23:42:01 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,4 +1,23 @@
 // Your code here...
-int a,b;
-scanf("%d%d",&a,&b);
-char ch='+','-','/','*';
+#include<stdio.h>
+int main(){
+    int a,b;
+    scanf("%d%d",&a,&b);
+    char ch[]={'+','-','/','*'};\
+    if(ch=='+'){
+        int sum=a+b;
+        printf("%d",sum);
+    }
+    else if(ch=='-'){
+        int sub=a-b;
+        printf("%d",sub);
+    }
+    else if(ch=='*'){
+        int into=a*b;
+        printf("%d",into);
+    }
+    else if(ch=='/'){
+        int divide=a/b;
+        printf("%d",divide);
+    }
+}
